dda8c8d3fd479071d3631d5fe4796a581a0955c8
Javier Soto <sotoseattle@gmail.com>
2020-07-28 12:30:33 -0700

    beer fix

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,25 +3,32 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
 
-  def verse(number), do: give(number) <> take(number)
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
 
-  defp give(0), do: "No more bottles of beer on the wall, no more bottles of beer.\n"
-  defp give(n), do: "#{bottles(n)} of beer on the wall, #{bottles(n)} of beer.\n"
+  def verse(n) do
+    """
+    #{bottles(n)} of beer on the wall, #{bottles(n)} of beer.
+    Take #{one_it(n - 1)} down and pass it around, #{bottles(n - 1)} of beer on the wall.
+    """
+  end
 
-  defp take(0), do: "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-  defp take(1), do: "Take it down and pass it around, no more bottles of beer on the wall.\n"
-  defp take(n), do: "Take one down and pass it around, #{bottles(n - 1)} of beer on the wall.\n"
-
+  defp bottles(0), do: "no more bottles"
   defp bottles(1), do: "1 bottle"
   defp bottles(n), do: "#{n} bottles"
+
+  defp one_it(0), do: "it"
+  defp one_it(_), do: "one"
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
 
-  def lyrics(), do: lyrics(99..0)
-
-  def lyrics(range) do
+  def lyrics(range \\ 99..0) do
     range |> Enum.map(&verse/1) |> Enum.join("\n")
   end
 end
